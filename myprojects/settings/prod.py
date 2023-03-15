706d2251391daa0ff1293a7784dfc844303cae5c
Sang jin An <photoan79@naver.com>
2023-03-15 06:45:20 -0700

    db change

--- myprojects/settings/prod.py
+++ myprojects/settings/prod.py
@@ -4,13 +4,3 @@ ALLOWED_HOSTS = [env('ALLOWED_HOSTS_PROD')]
 STATICFILES_DIRS = []
 DEBUG = False
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": 'django.db.backends.postgresql_psycopg2',
-#         'NAME': env('DB_NAME'),
-#         'USER': env('DB_USER'),
-#         'PASSWORD': env('DB_PASSWORD'),
-#         'HOST': env('DB_HOST'),
-#         'PORT': '5432',
-#     }
-# }
