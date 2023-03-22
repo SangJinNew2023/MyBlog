1574a92fb76fa7a5c70b11064dc960d6b91ffc25
Sang jin An <photoan79@naver.com>
2023-03-22 01:25:49 -0700

    admin change

--- myinventory/admin.py
+++ myinventory/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product, Order
 
 
-admin.site.site_header="MyInventory Dashboard" #change the admin page title
+admin.site.site_header="Myprojects Dashboard" #change the admin page title
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quantity')
